062415caa304b3d1b0ad6c43915ff8ab89c8c33e
andrey.oshkin <vchora96@yandex.ru>
2022-05-02 16:57:27 +0300

    refactor methods

---
+++ src/test/java/ru/oshkin/TestData.java
@@ -0,0 +1,12 @@
+package ru.oshkin;
+
+public class TestData {
+    public static final String NAME = "Жора";
+    public static final String BLOG_NAME = "Жора";
+    public static final String LATIN_NAME = "George";
+
+    public static final String SECOND_NAME = "Иванов";
+    public static final String LATIN_SECOND_NAME = "Ivanov";
+
+    public static final String DATE_OF_BIRTH = "13.11.2001";
+}
--- src/test/java/ru/oshkin/WebDriverHomeworkTest.java
+++ src/test/java/ru/oshkin/WebDriverHomeworkTest.java
@@ -10,8 +10,11 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.Calendar;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+
+import static ru.oshkin.TestData.*;
 
 public class WebDriverHomeworkTest {
     private WebDriver driver;
@@ -40,6 +43,15 @@ public class WebDriverHomeworkTest {
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 
+        logInByUser();
+        setPrivateDataInfo();
+        setMainInfo();
+        setContactInfo();
+
+        makeClick("//button[@title ='Сохранить и продолжить']", "Сохранить и продолжить");
+    }
+
+    private void logInByUser() {
         String link = "https://otus.ru";
         driver.get(link);
         logger.info("Перешли по ссылке");
@@ -70,83 +82,51 @@ public class WebDriverHomeworkTest {
         WebElement userButton2 = driver.findElement(By.xpath("//a[@title ='О себе']"));
         userButton2.click();
         logger.info("Открываем информацию о себе");
+    }
 
-        WebElement userButton3 = driver.findElement(By.xpath("//input [@name ='fname']"));
-        userButton3.clear();
-        userButton3.sendKeys("Жора");
-        logger.info("Очищаем поле имя и вводим новое имя");
+    private void findElemSetValue(String locator, String value, String fieldName) {
+        WebElement firstNameButton = driver.findElement(By.xpath(locator));
+        firstNameButton.clear();
+        firstNameButton.sendKeys(value);
+        logger.info(String.format("Очищаем поле: %s и вводим новое поле: %s", fieldName, fieldName));
+    }
 
-        WebElement userButton4 = driver.findElement(By.xpath("//input [@name ='fname_latin']"));
-        userButton4.clear();
-        userButton4.sendKeys("George");
-        logger.info("Очищаем поле имя и вводим новое имя");
-
-        WebElement userButton5 = driver.findElement(By.xpath("//input [@name ='lname']"));
-        userButton5.clear();
-        userButton5.sendKeys("Иванов");
-        logger.info("Очищаем поле фамилия и вводим новую фамилию");
-
-        WebElement userButton6 = driver.findElement(By.xpath("//input [@name ='lname_latin']"));
-        userButton6.clear();
-        userButton6.sendKeys("Ivanov");
-        logger.info("Очищаем поле фамилия и вводим новую фамилию");
-
-        WebElement userButton7 = driver.findElement(By.xpath("//input [@name ='blog_name']"));
-        userButton7.clear();
-        userButton7.sendKeys("Жора");
-        logger.info("Очищаем поле имя блога и вводим новое имя блога");
-
-        WebElement userButton8 = driver.findElement(By.xpath("//input [@name ='date_of_birth']"));
-        userButton8.clear();
-        userButton8.sendKeys("13.11.2001");
-        logger.info("Очищаем поле дата рождения и вводим новую дату рождения");
-
-        WebElement calendarInput = driver.findElement(By.xpath("//input[@title='День рождения']"));
+    private void makeClick(String locator, String fieldName) {
+        WebElement calendarInput = driver.findElement(By.xpath(locator));
         calendarInput.click();
-        logger.info("Закрываем всплывающее окно календарь");
+        logger.info(String.format("Кликаем на поле: %s", fieldName));
+    }
 
-        WebElement countryInput = driver.findElement(By.xpath("//div[@class ='input input_full lk-cv-block__input " +
-                "lk-cv-block__input_fake lk-cv-block__input_select-fake js-custom-select-presentation']"));
-        countryInput.click();
-        logger.info("Открываем выпадающий список стран");
+    private void setPrivateDataInfo() {
+        findElemSetValue("//input [@name ='fname']", NAME, "имя");
+        findElemSetValue("//input [@name ='fname_latin']", LATIN_NAME, "имя на английском");
+        findElemSetValue("//input [@name ='lname']", SECOND_NAME, "фамилия");
+        findElemSetValue("//input [@name ='lname_latin']", LATIN_SECOND_NAME, "фамилия на английском");
+        findElemSetValue("//input [@name ='blog_name']", BLOG_NAME, "имя в блоге");
+        findElemSetValue("//input [@name ='date_of_birth']", DATE_OF_BIRTH, "дата рождения");
 
-        WebElement selectedCountry = driver.findElement(By.xpath("//button[@title='Узбекистан']"));
-        selectedCountry.click();
-        logger.info("Выбираем страну");
+        makeClick("//input[@title='День рождения']", "окно календаря");
+    }
 
-        WebElement cityInput = driver.findElement(By.xpath("//div[@class ='input input_full lk-cv-block__input" +
-                " lk-cv-block__input_fake lk-cv-block__input_select-fake js-custom-select-presentation']/child::span[contains(text(),'Город')]"));
-        cityInput.click();
-        logger.info("Открываем выпадающий список городов");
+    private void setMainInfo() {
+        makeClick("//div[@class ='input input_full lk-cv-block__input lk-cv-block__input_fake lk-cv-block__input_select-fake " +
+                "js-custom-select-presentation']", "список стран");
+        makeClick("//button[@title='Узбекистан']", "Узбекистан");
+        makeClick("//div[@class ='input input_full lk-cv-block__input lk-cv-block__input_fake lk-cv-block__input_select-fake" +
+                " js-custom-select-presentation']/child::span[contains(text(),'Город')]", "список городов");
+        makeClick("//button[@title='Бухара']", "город");
+        makeClick("//input[@data-title ='Уровень знания английского языка']/following-sibling::div", "уровни языка");
+        makeClick("//button[@title='Начальный уровень (Beginner)']", "(Beginner)");
+    }
 
-        WebElement selectedCity = driver.findElement(By.xpath("//button[@title='Бухара']"));
-        selectedCity.click();
-        logger.info("Выбираем город");
+    private void setContactInfo() {
 
-        WebElement languageInput = driver.findElement(By.xpath("//input[@data-title ='Уровень знания английского языка']/following-sibling::div"));
-        languageInput.click();
-        logger.info("Открываем выпадающий список уровней знания английского языка");
+        makeClick("//button[contains(text(),'Указать телефон')]", "форма ввода телефона");
 
-        WebElement selectedLanguageLevel = driver.findElement(By.xpath("//button[@title='Начальный уровень (Beginner)']"));
-        selectedLanguageLevel.click();
-        logger.info("Выбираем уровень знания английского языка");
+        findElemSetValue("//Input[@name ='phone' and @placeholder ='Номер телефона']", "+7 911 111-11-11", "номер телефона");
 
-        WebElement phoneModalWindow = driver.findElement(By.xpath("//button[contains(text(),'Указать телефон')]"));
-        phoneModalWindow.click();
-        logger.info("Открываем модальное окно с формой ввода телефона");
-
-        WebElement phoneInput = driver.findElement(By.xpath("//Input[@name ='phone' and @placeholder ='Номер телефона']"));
-        phoneInput.clear();
-        phoneInput.sendKeys("+7 911 111-11-11");
-        logger.info("Вводим номер телефона");
-
-        WebElement sendButton = driver.findElement(By.xpath("//button[contains(text(),'Отправить')]"));
-        sendButton.click();
-        logger.info("Нажимаем кнопку отправить");
-
-        WebElement clouseButton = driver.findElement(By.xpath("//div[@class ='modal__close ic-close js-close-modal']"));
-        clouseButton.click();
-        logger.info("Нажимаем кнопку закрыть модальное окно");
+        makeClick("//button[contains(text(),'Отправить')]", "Отправить");
+        makeClick("//div[@class ='modal__close ic-close js-close-modal']", "модальное окно с телефоном");
 
 //        List<WebElement> elements10 = driver.findElements(By.xpath("//div[@data-prefix ='contact']/descendant::button[contains(text(),'Удалить')]"));
 //        for (int i = 0; i < elements10.size(); i++) {
@@ -158,49 +138,33 @@ public class WebDriverHomeworkTest {
 //            }
 //        }
 
-        driver.findElement(By.cssSelector("button.js-lk-cv-custom-select-add")).click();
-        logger.info("Добавить контакт");
-
-        WebElement wayCommunication = driver.findElement(By.xpath("//span[contains(text(),'Способ связи')]"));
-        wayCommunication.click();
-        logger.info("Открываем выбор способа связи");
-
-        List<WebElement> elements = driver.findElements(By.xpath("//div[@class ='lk-cv-block__select-options " +
-                "lk-cv-block__select-options_left js-custom-select-options-container']/descendant::button[@title='Viber']"));
-        elements.get(0).click();
-        logger.info("Выбираем способ связи Viber");
-
-        WebElement firstContactInput = driver.findElement(By.xpath("//input[@id='id_contact-0-value']"));
-
-        firstContactInput.sendKeys("+7 966 666-66-66");
-        logger.info("Заполняем поле ввода Viber");
-
-        WebElement firstContactInput3 = driver.findElement(By.xpath("//input[@id='id_contact-0-value']"));
-        firstContactInput3.click();
-        logger.info("Закрываем поле выбора способа связи");
 
         driver.findElement(By.cssSelector("button.js-lk-cv-custom-select-add")).click();
         logger.info("Добавить контакт");
 
-        WebElement wayCommunication2 = driver.findElement(By.xpath("//span[contains(text(),'Способ связи')]"));
-        wayCommunication2.click();
-        logger.info("Открываем выбор способа связи");
+        makeClick("//span[contains(text(),'Способ связи')]", "Способ связи");
 
-        List<WebElement> elements2 = driver.findElements(By.xpath("//div[@class ='lk-cv-block__select-scroll" +
-                " lk-cv-block__select-scroll_service js-custom-select-options']/child::button[@title ='Skype']"));
-        elements2.get(1).click();
-        logger.info("Выбираем способ связи Skype");
+        selectCommunicationWay("//div[@class ='lk-cv-block__select-options " +
+                "lk-cv-block__select-options_left js-custom-select-options-container']/descendant::button[@title='Viber']", "Viber");
 
-        WebElement firstContactInput2 = driver.findElement(By.xpath("//input[@id='id_contact-1-value']"));
-        firstContactInput2.sendKeys("Test_login");
-        logger.info("Заполняем поле ввода Skype");
+        findElemSetValue("//input[@id='id_contact-0-value']", "+7 966 666-66-66", "Viber контакт");
+        makeClick("//input[@id='id_contact-0-value']", "выбор способа связи");
 
-        WebElement firstContactInput4 = driver.findElement(By.xpath("//input[@id='id_contact-1-value']"));
-        firstContactInput4.click();
-        logger.info("Закрываем поле выбора способа связи");
+        driver.findElement(By.cssSelector("button.js-lk-cv-custom-select-add")).click();
+        logger.info("Добавить контакт");
 
-        WebElement saveButton = driver.findElement(By.xpath("//button[@title ='Сохранить и продолжить']"));
-        saveButton.click();
-        logger.info("Нажимаем кнопку сохранить и продолжить");
+        makeClick("//span[contains(text(),'Способ связи')]", "способ связи");
+        selectCommunicationWay("//div[@class ='lk-cv-block__select-scroll " +
+                "lk-cv-block__select-scroll_service js-custom-select-options']/child::button[@title ='Skype']", "Skype");
+
+        findElemSetValue("//input[@id='id_contact-1-value']", "Test_login", "логин Skype");
+
+        makeClick("//input[@id='id_contact-1-value']", "способ связи");
+    }
+
+    private void selectCommunicationWay(String locator, String name) {
+        List<WebElement> communicationWays = driver.findElements(By.xpath(locator));
+        communicationWays.get(1).click();
+        logger.info(String.format("Выбираем способ связи %s"), name);
     }
 }
